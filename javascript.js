5e01aa33801195da42d938cc2bb036333193ac0e
AttackSwan <124640611+AttackSwan@users.noreply.github.com>
2023-05-09 15:41:41 +1000

    Add and style form inputs

--- javascript.js
+++ javascript.js
@@ -4,6 +4,6 @@ const form = document.querySelector(".form");
 formButton.onclick = () => openForm();
 
 function openForm() {
-  form.style.display = "block";
-  document.querySelector(".container").style.filter = "blur(4px)";
+	form.style.display = "block";
+	document.querySelector(".container").style.filter = "blur(4px)";
 }
