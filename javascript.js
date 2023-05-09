c1929bb67813d7728950339ca043624441732a23
AttackSwan <124640611+AttackSwan@users.noreply.github.com>
2023-05-09 16:28:48 +1000

    Add bookshelves

--- javascript.js
+++ javascript.js
@@ -4,6 +4,8 @@ const form = document.querySelector(".form");
 formButton.onclick = () => openForm();
 
 function openForm() {
+	console.log("Click");
 	form.style.display = "block";
 	document.querySelector(".container").style.filter = "blur(4px)";
+	formButton.disabled = true;
 }
