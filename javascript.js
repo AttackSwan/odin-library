8b2ca5728d8923f43d07ab8715b759d890aa0ac1
AttackSwan <124640611+AttackSwan@users.noreply.github.com>
2023-05-11 15:42:02 +1000

    Add book display function

--- javascript.js
+++ javascript.js
@@ -16,10 +16,7 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(data) {
-	let isRead = true;
-	if (data.get("book-read") === null) {
-		isRead = false;
-	}
+	const isRead = data.get("book-read") !== null;
 	const newBook = new Book(
 		data.get("book-title"),
 		data.get("book-author"),
@@ -27,7 +24,7 @@ function addBookToLibrary(data) {
 		isRead
 	);
 	myLibrary.push(newBook);
-	console.log(myLibrary);
+	console.log(newBook);
 }
 
 function showForm() {
@@ -48,17 +45,20 @@ function submitForm(e) {
 	addBookToLibrary(data);
 }
 
-function addBook() {
-	let arrayIndex = myLibrary.length;
-	console.log(arrayIndex);
+function displayBooks() {
+	//for each book in the array
+	myLibrary.forEach(addToDom);
+	//add the book to the Dom
+	// let arrayIndex = myLibrary.length;
+	// console.log(arrayIndex);
 
-	let book = document.createElement("div");
-	book.classList.add("book");
-	book.setAttribute("data-index", arrayIndex);
-	books.appendChild(book);
-	myLibrary.push(book);
+	// let book = document.createElement("div");
+	// book.classList.add("book");
+	// book.setAttribute("data-index", arrayIndex);
+	// books.appendChild(book);
+	// myLibrary.push(book);
 
-	console.log(myLibrary);
+	// console.log(myLibrary);
 	// for (i = 0; i < (size * size); i++){
 	//     let cell = document.createElement('div');
 	//     cell.classList.add("grid-item");
@@ -69,3 +69,56 @@ function addBook() {
 	//     });
 	//     grid.appendChild(cell);
 }
+
+function addToDom(book) {
+	const newBook = document.createElement("div");
+	newBook.classList.add("book");
+
+	const titleDiv = createDivWithContent("title", book.title);
+	const authorDiv = createDivWithContent("author", book.author);
+	const pagesDiv = createDivWithContent("pages", book.pages + " pages");
+
+	newBook.append(
+		titleDiv,
+		authorDiv,
+		pagesDiv,
+		createDiv("stripe"),
+		createDiv("bookRead"),
+		createDiv("bookSide")
+	);
+
+	books.appendChild(newBook);
+	console.log(myLibrary);
+}
+
+function createDiv(className) {
+	const div = document.createElement("div");
+	div.classList.add(className);
+	return div;
+}
+
+function createDivWithContent(className, content) {
+	const div = createDiv(className);
+	div.appendChild(document.createTextNode(content));
+	return div;
+}
+
+function initShelf() {
+	const initBook1 = new Book(
+		"The Fellowship of the Ring",
+		"J. R. R. Tolkien",
+		423,
+		true
+	);
+	const initBook2 = new Book(
+		"Harry Potter and the Philosopher's Stone",
+		"J. K. Rowling",
+		223,
+		true
+	);
+	const initBook3 = new Book("Heir to the Empire", "Timothy Zhan", 416, true);
+	myLibrary.push(initBook1, initBook2, initBook3);
+	displayBooks();
+}
+
+initShelf();
